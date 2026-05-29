6ac902f70bf004fc446e184c7f7c9888f8f5e7fa
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-05-29 16:27:14 -0300

    feat: Inserindo regras para acesso de login

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Controller('users')
 export class UsersController {
@@ -12,10 +21,12 @@ export class UsersController {
     return this.usersService.create(data);
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('ADMIN')
+@Get()
+findAll() {
+  return this.usersService.findAll();
+}
 
   @UseGuards(JwtAuthGuard)
   @Get('perfil')
