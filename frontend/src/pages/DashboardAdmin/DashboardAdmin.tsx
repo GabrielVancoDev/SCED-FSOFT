15e8067bb6490314fbc8894d228b36b599bac265
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-06-05 09:48:26 -0300

    feat: Implementando logout de usuários

--- frontend/src/pages/DashboardAdmin/DashboardAdmin.tsx
+++ frontend/src/pages/DashboardAdmin/DashboardAdmin.tsx
@@ -1,11 +1,22 @@
 import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function DashboardAdmin() {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div>
       <h1>Dashboard Administrador</h1>
+
+      <p>Bem-vindo, {user?.email}</p>
+
+      <button onClick={handleLogout}>Sair</button>
     </div>
   );
 }
--- frontend/src/pages/DashboardUser/DashboardUser.tsx
+++ frontend/src/pages/DashboardUser/DashboardUser.tsx
@@ -1,66 +1,22 @@
-import { createContext, useContext, useState } from "react";
-
-// A Interface é responsável por definir os tipos de dados que serão usados na aplicação
-interface User {
-  id: number;
-  email: string;
-  tipoUsuario: string;
-}
-
-//
-interface AuthContextData {
-  user: User | null;
-  token: string | null;
-  login: (token: string, user: User) => void;
-  logout: () => void;
-}
-
-// Cria o contexto
-const AuthContext = createContext<AuthContextData>({} as AuthContextData);
-
-// Cria o provider
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<User | null>(null); // Exportando o contexto
-  const [token, setToken] = useState<string | null>(null);
-
-  function login(token: string, user: User) {
-    localStorage.setItem("token", token);
-    localStorage.setItem("user", JSON.stringify(user));
-
-    setToken(token);
-    setUser(user);
-  }
-
-  function logout() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-
-    setToken(null);
-    setUser(null);
-  }
-
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        token,
-        login,
-        logout,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-}
-
-export function useAuth() {
-  return useContext(AuthContext);
-}
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function DashboardUser() {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    logout();
+    navigate("/");
+  }
+
   return (
     <div>
       <h1>Dashboard Usuário</h1>
+
+      <p>Bem-vindo, {user?.email}</p>
+
+      <button onClick={handleLogout}>Sair</button>
     </div>
   );
 }
